d25c20a95ec2018dcc6bbcc36db571dcd65ffe6c
Arı Bilgi <nomail@aribilgi.com.tr>
2024-05-02 12:00:40 +0300

    Some changes

--- EmployeeApi/Controllers/EmployeeController.cs
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -26,6 +26,35 @@ namespace EmployeeApi.Controllers
             return Ok(await _context.Employees.ToListAsync());
         }
 
+        // 2. Employees kayıtları arasından Id si belirlenmiş bir kayıdı getirme işlemi
+        // GET: api/<EmployeeController>/5 --> Id=5 olan kaydı getir.
+        [HttpGet("id")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            // Üzerine gelen id bilgisine göre ilgili tablodan istenen kayıdı bulma işlemi
+            var employee= await _context.Employees.FindAsync(id);
+
+            if (employee == null) 
+            {
+                // herhangi bir sekilde employee içerği oluşmamışsa(kayıt bulunamamışsa)
+                return BadRequest("Employee kaydı bulunamadı....");
+            }
+
+            return Ok(employee); // 200 kodu - başarılı işlem
+
+        }
+
+        // 3. Employee tablosu üzerinde yeni bir kayıt ekleme işlemi
+        // POST: api/<EmployeeController>
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee); // EF sağolsun..
+
+            await _context.SaveChangesAsync(); // db tablosu üzerinde kalıcı olması için
+
+            return Ok(await _context.Employees.ToListAsync());
+        }
 
     }
 }
