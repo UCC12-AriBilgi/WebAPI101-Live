f2c625df777c123528c91e39e7c25722162c346e
Arı Bilgi <nomail@aribilgi.com.tr>
2024-05-02 14:51:01 +0300

    Some changes

--- EmployeeWeb/Controllers/EmployeeController.cs
+++ EmployeeWeb/Controllers/EmployeeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeWeb.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeWeb.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        // Burası bizim sayfalarımızı kontrol edecek controller
+
+        private readonly IEmployeeService _service;
+        private readonly string _ApiBase;
+        private readonly IWebHostEnvironment _environment; // iki uygulama arası geliş gidiş olacağı için konuldu.
+
+        public EmployeeController(IEmployeeService service,IConfiguration configuration,IWebHostEnvironment environment)
         {
-            return View();
+            _service= service;
+            _ApiBase = configuration["APISection:BaseAddress"];
+            _environment = environment;
+        }
+
+        // Tüm kayıtları listeleme kısmı
+        public async Task<IActionResult> Index()
+        {
+            var employees= await _service.GetAll();
+
+            return View(employees);
         }
     }
 }
